e5a016b48017082568eede86e00c58d8af4e4d0e
chichichi1310 <chisatokyoda@icloud.com>
2025-11-12 20:36:13 +0900

    Add files via upload

---
+++ kadai_015/Car_Chapter15.java
@@ -0,0 +1,40 @@
+package kadai_015;
+
+public class Car_Chapter15 {
+    //フィールド定義
+	private int    gear = 1;
+	private int    speed = 10;
+
+    public void changeGear(int afterGear) {
+    	 this.gear = afterGear;
+    	 switch(this.gear) {
+    	 
+    	 case 1:
+    		 this.speed = 10;
+    		 break;
+    	 case 2:
+    		 this.speed = 20;
+    		 break;
+    	 case 3:
+    		 this.speed = 30;
+    		 break;
+    	 case 4:
+    		 this.speed = 40;
+    		 break;
+    	 case 5:
+    		 this.speed = 50;
+    		 break;
+    	 };
+    	
+     System.out.println("ギア1から" + this.gear + "に切り替えました");
+    	
+    }
+    public void run() {
+
+            System.out.println("速度は時速" + this.speed + "kmです");
+    	
+    	
+    }
+    
+    
+}
