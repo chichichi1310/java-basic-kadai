993b878b3a979410322fafad7d8d000afda321ff
chichichi1310 <chisatokyoda@icloud.com>
2025-11-19 20:53:22 +0900

    Add files via upload

---
+++ kadai_018/KatoExec_Chapter18.java
@@ -0,0 +1,17 @@
+package kadai_018;
+
+public class KatoExec_Chapter18 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+        KatoTaro_Chapter18 taro = new KatoTaro_Chapter18();
+        taro.execIntroduce();
+
+        KatoIchiro_Chapter18 Ichiro = new KatoIchiro_Chapter18();
+        Ichiro.execIntroduce();
+        
+        KatoHanako_Chapter18 Hanako = new KatoHanako_Chapter18();
+        Hanako.execIntroduce();
+	}
+
+}
