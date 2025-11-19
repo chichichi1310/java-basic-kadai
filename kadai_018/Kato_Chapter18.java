7eca9b52cebf61d7cce68f35002f71db343a2f8a
chichichi1310 <chisatokyoda@icloud.com>
2025-11-19 21:53:33 +0900

    Add files via upload

---
+++ kadai_018/Kato_Chapter18.java
@@ -0,0 +1,23 @@
+package kadai_018;
+
+public abstract class Kato_Chapter18 {
+    public String 	familyName = "加藤";
+    public String 	givenName = "";
+    public String 	address = "住所は東京都中野区〇×";
+    
+    public void commonIntroduce(){
+        System.out.println("名前は" + this.familyName + this.givenName + "です");
+        System.out.println(this.address + "です");
+    }
+    
+    abstract public void eachIntroduce();
+    
+    public void execIntroduce() {
+        this.commonIntroduce();   // 共通部分
+        this.eachIntroduce();     // 個別の自己紹介
+        System.out.println();
+    	
+    }
+    
+    
+}
